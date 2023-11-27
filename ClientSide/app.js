d02b2e4ec0bed058088616be75a179d18781a71c
jaison10 <jaisonj1010@gmail.com>
2023-11-27 02:07:14 +0000

    client side and server node

--- ClientSide/app.js
+++ ClientSide/app.js
@@ -1,4 +1,5 @@
 window.onload = function() {
+    // my current location
     currentLoc = getLocation();
 
     const map = new google.maps.Map(document.getElementById("map"), {
@@ -7,22 +8,22 @@ window.onload = function() {
     });
 };
 
-  // Obtain current location co-ordinates
-  function getLocation() {
+// Obtain current location co-ordinates
+function getLocation() {
     if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
+        navigator.geolocation.getCurrentPosition(
         function(position) {
-          const latitude = position.coords.latitude;
-          const longitude = position.coords.longitude;
-          console.log(`Latitude: ${latitude}, Longitude: ${longitude}`);
-          return [latitude, longitude];
-          // You can use the latitude and longitude values as needed
+        const latitude = position.coords.latitude;
+        const longitude = position.coords.longitude;
+        console.log(`Latitude: ${latitude}, Longitude: ${longitude}`);
+        return [latitude, longitude];
+        // You can use the latitude and longitude values as needed
         },
         function(error) {
-          console.error("Error getting user location:", error);
+        console.error("Error getting user location:", error);
         }
-      );
+        );
     } else {
-      console.log("Geolocation is not supported by this browser.");
+    console.log("Geolocation is not supported by this browser.");
     }
-  }
+}
